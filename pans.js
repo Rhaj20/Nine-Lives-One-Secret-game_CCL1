e862fb25b8628337114e0ee606d42186cd609ef7
Hajar Rhachi <162902707+Rhaj20@users.noreply.github.com>
2025-12-30 22:33:28 +0100

    Add files via upload

---
+++ pans.js
@@ -0,0 +1,21 @@
+import { global } from "../modules/global.js";
+import { BaseGameObject } from "./baseGameObject.js";
+
+class Pans extends BaseGameObject{
+    blockGravityForces = true;
+
+    reactToCollision = function (collidingObject) {
+        if (collidingObject.name == "Cat") {
+            collidingObject.x = collidingObject.previousX;
+            collidingObject.y = collidingObject.previousY;
+            global.health.value -= 1;
+        }
+    }
+
+    constructor (x, y, width, height) {
+        super(x, y, width, height);
+        this.loadImages(["./images/pan.png"]);
+    }
+}
+
+export{Pans}
